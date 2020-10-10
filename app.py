3cfc5cd423dd2f000249d4f107b1ad664340b128
donaldferguson <donff2@aol.com>
2020-10-10 16:14:49 -0400

    Project template

---
+++ app.py
@@ -0,0 +1,50 @@
+
+# Import functions and objects the microservice needs.
+# - Flask is the top-level application. You implement the application by adding methods to it.
+# - Response enables creating well-formed HTTP/REST responses.
+# - requests enables accessing the elements of an incoming HTTP/REST request.
+#
+import json
+# Setup and use the simple, common Python logging framework. Send log messages to the console.
+# The application should get the log level out of the context. We will change later.
+#
+
+import os
+import sys
+
+from flask import Flask, Response
+from flask import request
+import pymysql
+from datetime import datetime
+
+c_info = {
+        "user": "root",
+        "password": "dbuserdbuser",
+        "cursorclass": pymysql.cursors.DictCursor,
+    }
+
+
+application = Flask(__name__)
+
+
+# This function performs a basic health check. We will flesh this out.
+@application.route("/health", methods=["GET"])
+def health_check():
+
+    rsp_data = { "status": "healthy", "time": str(datetime.now()) }
+    rsp_str = json.dumps(rsp_data)
+    rsp = Response(rsp_str, status=200, content_type="application/json")
+    return rsp
+
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+
+
+
+
+
+    application.debug = True
+    application.run(host='0.0.0.0', port=5000)
